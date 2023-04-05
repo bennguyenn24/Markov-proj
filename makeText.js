b3dedc3c4b2d59ccd825af7855b889945ebca92d
Ben Nguyen <bennguyenn24@gmail.com>
2023-04-05 15:36:23 -0700

    Generated pathway and API

--- makeText.js
+++ makeText.js
@@ -1,2 +1,11 @@
 /** Command-line tool to generate Markov text. */
+const fs = require("fs");
+const axios = require("axios");
+const process = require("process");
+const { markov } = require("./markov");
 
+function generateText() {
+    console.log('hello dog')
+}
+
+generateText();
